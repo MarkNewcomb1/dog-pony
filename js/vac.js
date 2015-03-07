aa5929e2a9fb982e0ac3e1b4c9a974360eb0d79b
Nathan White <nw@nwhite.net>
2015-03-06 17:15:15 -0700

    added vac.js back using reference to key file

---
+++ js/vac.js
@@ -0,0 +1,174 @@
+$(function() {
+
+	var locations = [
+		{
+			"name":"South Pacific",
+			"terms":"south pacific wallpaper",
+			"weatherId": "Noumea",
+			"blurb": "<p> Located in the southwest Pacific Ocean, New Caledonia is the third largest island in the Pacific. Offering one beautiful beach after another, immerse yourself in crystal clear turquoise lagoons and vast open spaces. </p> <p> The capital, Nouméa is a little piece of France in the Pacific, with its French Riviera atmosphere and its luxury boutiques. With an abundance of mixed cultural influences, New Caledonia blends together heritage, entertainment and leisure activities. </p> <p> Grande Terre, the main island boasts rugged mountains, waters and the world’s largest coral lagoon, nearby lies, Île des Pins, with slender pines, white sand beaches and an emerald lagoon. New Caledonia is surrounded by the world’s largest enclosed lagoon, where you will find a multitude of sea animals, canyons, caves and coral just waiting to be explored. </p> <p> For the avid sailor, the trade winds will carry you on an adventure to deserted islands, for some exotic solitude. Other activities include, water sports, diving, hiking, fishing, golfing, and hunting. </p> <p> The climate is temperate with two main seasons, the cooler season is June to August and warmer is mid-November to mid-April. During the cooler season, it is around 15°C (59°F) at night and around 24°C (73°F) during the day. During the warmer season the maximum temperatures are between 26°C (79°C) and 30°C (86°F). </p> <p> New Caledonia is a hotspot of global biodiversity. Nowhere else on the planet, will you find an area as small with as many species. Detached, about 60 million years ago, from the supercontinent of Gondwana, which included Australia and even Antarctica, New Caledonia was similar to a Noah's ark carrying part of the vegetation and elements of fauna of that period. This explains the unique endemic flora of New Caledonia and the endemism of some of its fauna. Do not leave without exploring all these amazing gardens! </p> <p> Caledonian fauna is particularly unique and above all friendly. Species are sometimes surprising but never dangerous to humans. Some 2,535 species are currently listed in New Caledonia, of which 300 are endemic. </p> <p> Visiting New Caledonia, and discovering its abundant nature, is like having the privilege of going back in time millions of years when man did not exist and when the dinosaurs dominated the world. Here in particular you can still find: Amborella the oldest plants on the planet going back 130 million years. </p> <p> <a href='http://en.visitnewcaledonia.com/'>Visit New Caledonia Tourism Site</a> </p>"},
+		{
+			"name":"Miami",
+			"terms":"Miami beaches wallpaper",
+			"weatherId": "Miami,FL"			,
+			"blurb": "<p>Greater Miami is a rare destination where the cutting edge of urban chic co-exists with the beauty and splendour of a tropical paradise. Great year-round weather, top-ranked beaches and the sparkling waters of Biscayne Bay are the backdrop for a cosmopolitan metropolis, pulsing with the rhythms of its diverse population. Boasting a vibrant arts and cultural landscape, renowned nightlife and world-class dining, Miami draws an eclectic mix of visitors from sizzling celebrities to frolicking families seeking fun in the sun.</p>"
+		},
+		{
+			"name":"New York City",
+			"terms":"New York City Skyline wallpaper",
+			"blurb": "<p>New York City has been described as the cultural capital of the world by the diplomatic consulates of Iceland and Latvia and by New York's Baruch College. A book containing a series of essays titled New York, culture capital of the world, 1940–1965 has also been published as showcased by the National Library of Australia. In describing New York, author Tom Wolfe said, 'Culture just seems to be in the air, like part of the weather.'</p><p>Numerous major American cultural movements began in the city, such as the Harlem Renaissance, which established the African-American literary canon in the United States.[355][356] The city was a center of jazz[357] in the 1940s, abstract expressionism in the 1950s, and the birthplace of hip hop in the 1970s. The city's punk and hardcore scenes were influential in the 1970s and 1980s, and the city has long had a flourishing scene for Jewish American literature.</p><p>The city is the birthplace of many cultural movements, including the Harlem Renaissance in literature and visual art; abstract expressionism (also known as the New York School) in painting; and hip hop,[150] punk, salsa, disco, freestyle, Tin Pan Alley, and Jazz in music. New York City has been considered the dance capital of the world.[361][362][363] The city is also widely celebrated in popular lore, frequently the setting for books, movies (see List of films set in New York City), and television programs. New York has also frequently been ranked the top fashion capital of the world on the annual list compiled by the Global Language Monitor;the city's fashion industry provides approximately 180,000 employees with $11 billion in annual wages.</p>"
+		},
+		{
+			"name":"Paris",
+			"terms":"Paris wallpaper",
+			"blurb": "<p>A kids’ visit to the Louvre, an introduction to contemporary art at the Pompidou Centre, scientific workshops at the Cité des sciences et de l’industrie, tales and stories at Château Breteuil... In the museums, sites and monuments of Paris and its region, a wide range of leisure and cultural activities are offered throughout the year to children accompanied by their parents.</p><p>For those who prefer outdoor activities, the choice is equally attractive... Judge for yourself! What would you prefer: a visit to Disneyland Paris, a pony ride in the Jardin du Luxembourg, a trip on the enchanted river at the Jardin d’acclimatation, a picnic in the beautiful Domaine de Chamarande or climbing to the 4th floor of the Eiffel Tower?</p><p>To further appreciate the many facets of ‘Paris en famille’ discover different and amusing methods of transport (water bus, tourist train, Citroën 2CV...), which are available for you to discover the city and its region. And the good news is: your kids will love them! Enjoy your visit</p>"
+		},
+		{
+			"name":"London",
+			"terms":"London wallpaper",
+			"blurb": "<p>London is home to many museums, galleries, and other institutions, many of which are free of admission charges and are major tourist attractions as well as playing a research role. The first of these to be established was the British Museum in Bloomsbury, in 1753. Originally containing antiquities, natural history specimens and the national library, the museum now has 7 million artefacts from around the globe. In 1824 the National Gallery was founded to house the British national collection of Western paintings; this now occupies a prominent position in Trafalgar Square.</p><p>In the latter half of the 19th century the locale of South Kensington was developed as 'Albertopolis', a cultural and scientific quarter. Three major national museums are there: the Victoria and Albert Museum (for the applied arts), the Natural History Museum and the Science Museum. The National Portrait Gallery was founded in 1856 to house depictions of figures from British history; its holdings now comprise the world's most extensive collection of portraits.[267] The national gallery of British art is at Tate Britain, originally established as an annexe of the National Gallery in 1897. The Tate Gallery, as it was formerly known, also became a major centre for modern art; in 2000 this collection moved to Tate Modern, a new gallery housed in the former Bankside Power Station.</p>"
+		},
+		{
+			"name":"Dubai",
+			"terms":"Dubai wallpaper",
+			"blurb": "<p>Dubai is located on the southeast coast of the Persian Gulf and is one of the seven emirates that make up the country. Abu Dhabi and Dubai are the only two emirates to have veto power over critical matters of national importance in the country's legislature. The city of Dubai is located on the emirate's northern coastline and heads up the Dubai-Sharjah-Ajman metropolitan area. Dubai is to host World Expo 2020.</p><p>Dubai has emerged as a global city and business hub of the Persian Gulf region. It is also a major transport hub for passengers and cargo. By the 1960s Dubai's economy was based on revenues from trade and, to a smaller extent, oil exploration concessions, but oil was not discovered until 1966. Oil revenue first started to flow in 1969.[7] Dubai's oil revenue helped accelerate the early development of the city, but its reserves are limited and production levels are low: today, less than 5% of the emirate's revenue comes from oil.[8] The emirate's Western-style model of business drives its economy with the main revenues now coming from tourism, aviation, real estate, and financial services.[9][10][11] Dubai has recently attracted world attention through many innovative large construction projects and sports events. The city has become symbolic for its skyscrapers and high-rise buildings, in particular the world's tallest building, the Burj Khalifa. Dubai has been criticised for human rights violations concerning the city's largely South Asian workforce. Dubai's property market experienced a major deterioration in 2008–2009 following the financial crisis of 2007–2008, but is making a gradual recovery with help from neighbouring emirates.</p>"
+		}
+	];
+
+
+
+
+
+	var menuOpen = function() {
+		$('nav').animate({ bottom: '0px' }, 1500, 'easeOutBounce')
+	}
+
+	setTimeout(menuOpen,2700);
+
+
+
+	var bingResults = '';
+	var weatherResults = '';
+	var blurb = '';
+	var locationsIndex = '';
+	var destination = '';
+
+	$('nav').on('click','.locationLink', function() {
+		if($(this).attr('data-terms') !== destination) {
+			bingResults = '';
+			weatherResults = '';
+			locationsIndex = '';
+			$('.messaging').empty();
+			searches.searchAll($(this));
+		} else {
+			layout.process();
+		}
+
+	});
+
+	var searches = {
+
+		bingImagesUrl : "https://api.datamarket.azure.com/Bing/Search/v1/Image",
+		wikiUrl : '',
+		weatherUrl : 'http://api.worldweatheronline.com/free/v2/weather.ashx',
+		searchAll : function(target) {
+			var t = $(target);
+			var location = t.attr('data-terms');
+			var weather =  t.attr('data-weather');
+			locationsIndex = t.attr('data-index');
+			if($('h1')) this.setStatus('Searching...');
+			if(bingResults == ''){
+				this.bingImg(location);
+			} else {
+				layout.processBing();
+			}
+			if(weatherResults == ''){
+				this.weatherSearch(weather);
+			} else {
+				layout.processWeather();
+			}
+			if(blurb == '') {
+				layout.processBlurb();
+			}
+		},
+		bingImg : function(l) {
+			var theKey = api_keys.bing;
+			var encodedKey = btoa(theKey);
+			$.ajax({
+			  type: "GET",
+			  dataType: "json",
+			  headers: {
+			  	"Authorization":"Basic " + encodedKey
+			  },
+			  url: searches.bingImagesUrl,
+			  data: { Query: "'" + l + "'", $format: 'json', ImageFilters: "'Size:Large+Aspect:Wide'", Adult: "'Strict'" },
+			  success: function(data) {
+			  	bingResults = data;
+			  	layout.processBing();
+			  }
+			});
+		},
+		wikiSearch : function() {
+			// console.log('wiki');
+		},
+		weatherSearch : function(w) {
+			$.ajax({
+				url: searches.weatherUrl,
+				type: "GET",
+				dataType: "json",
+				data: {q:w,format:"json",num_of_days: 1,key: api_keys.weather },
+				success: function(data) {
+					weatherResults = data;
+					layout.processWeather();
+				}
+			})
+		},
+
+		setStatus : function(text) {
+			$('h1').text(text);
+		},
+		clearStatus : function() {
+			$('h1').remove();
+		}
+	}
+
+	var layout = {
+		processBing : function() {
+			var index = Math.floor(Math.random() * 50) + 1
+			var url = bingResults.d.results[index].MediaUrl;
+			this.showBing(url);
+		},
+		showBing: function(url) {
+			$('.hero').hide().css(
+				{
+					"background-image":"url(" + url + ")",
+					"background-repeat":"no-repeat",
+					"background-size":"cover",
+					"background-color": "transparent"
+				}
+			).fadeIn(function() {
+				$('video#bgvid,h1,.filter').remove();
+			});
+		},
+		processWeather: function() {
+			var weather = {
+				temp : weatherResults.data.current_condition[0].temp_F,
+				text : weatherResults.data.current_condition[0].weatherDesc[0].value,
+				icon : weatherResults.data.current_condition[0].weatherIconUrl[0].value,
+				name : weatherResults.data.request[0].query
+			}
+
+			$(".name").text(weather.name);
+			$(".temp").html(weather.temp + "&deg;");
+			$(".icon").empty().append("<img src='" + weather.icon + "' alt='weather icon' />");
+			$(".text").text(weather.text);
+			$('.weather').fadeIn();
+		},
+		processBlurb: function() {
+			setTimeout(function() {
+				$('.messaging').html(locations[locationsIndex].blurb).animate({
+					"right":"0px"
+				},1500,"easeOutBounce");
+			},1000);
+		}
+	}
+});
